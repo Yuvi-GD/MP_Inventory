de192a9cead2104256e11bbc7fa3ac8886475c00
Yuvi <156796059+Yuvi-GD@users.noreply.github.com>
2026-07-06 16:00:18 +0530

    Remove: MP_Trading module
    - now only available at trading-module branch separately

--- Source/MP_Trading/Private/Interfaces/MP_Inventory_PlayerState_I.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2026 UVSquare. All Rights Reserved.
-
-
-#include "Interfaces/MP_Inventory_PlayerState_I.h"
-
-// Add default functionality here for any IMP_Inventory_PlayerState functions that are not pure virtual.
--- Source/MP_Trading/Private/Interfaces/MP_TradeNotification_I.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2026 UVSquare. All Rights Reserved.
-
-
-#include "Interfaces/MP_TradeNotification_I.h"
-
-// Add default functionality here for any IMP_TradeNotification_I functions that are not pure virtual.
